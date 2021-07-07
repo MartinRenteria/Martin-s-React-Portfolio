bab537e8cde7727cd58cce445db538ded49dbb90
Martin Renteria <renteriamp96@gmail.com>
2021-07-06 23:44:49 -0700

    Adding bootstrap framework to react project portfolio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from "./components/Home/home";
 import './App.css';
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <NavBar />
     </div>
   );
 }
