e8ab6dd203cb3b0b138ddd09ecc32083f0a305ba
Martin Renteria <renteriamp96@gmail.com>
2021-07-07 18:25:44 -0700

    Added additional styling to typewriter effect

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./home.css"
 import Typewriter from "typewriter-effect";
 
 
@@ -8,7 +9,7 @@ function Home() {
         options={{ delay: 50 }}
         onInit={(typewriter) => {
           typewriter.start()
-            .typeString(`Hi there, I'm Martin Renteria.<br/>A JavaScript developer<br/>based in Bellevue, WA.`)
+            .typeString(`Hi there, I'm Martin Renteria.<br/>A JavaScript developer<br/>based in Bellevue, WA.<br/> Check out my work! =)`)
         }}
       />
     )
