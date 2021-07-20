c3db12d29784a6e39629089acb28e4dca47d33c5
Martin Renteria <renteriamp96@gmail.com>
2021-07-19 22:58:55 -0700

    Adding skills section. Still need to add fab icons

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Contact from './components/Contact/contact';
 import Home from "./components/Home/home";
 import NavBar from "./components/NavBar/NavBar";
+import Skills from './components/Skills/skills';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Home />
+      <Skills />
       <Contact />
     </div>
   );
---
+++ src/components/Skills/skills.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Skills() {
+    return (
+<section class="content container row bg-dark px-1 py-3 mx-0" id="skill">
+<h2 class="section-title" id="skill-title"><strong>My skills</strong></h2>
+<div class="skills">
+    <div class="skill-row justify-content-center" id="skill-row">
+        <i class="skill-icon fab fa-js-square"></i>
+        <i class="skill-icon fab fa-css3-alt"></i>
+        <i class="skill-icon fab fa-html5"></i>
+        <i class="skill-icon fab fa-bootstrap"></i>
+        <i class="skill-icon fab fa-node"></i>
+        <i class="skill-icon fab fa-npm"></i>
+        <i class="skill-icon fab fa-react"></i>
+        <i class="skill-icon fas fa-database"></i>
+        <i class="skill-icon fab fa-java"></i>          
+     </div>
+</div> 
+</section>
+    )
+}
+
+export default Skills;
