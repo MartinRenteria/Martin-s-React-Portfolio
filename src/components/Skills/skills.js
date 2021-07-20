ca377ab116dab16fff4a6c28d7d8a7b185ba2f14
Martin Renteria <renteriamp96@gmail.com>
2021-07-20 10:56:17 -0700

    Added additional skills to skillset sections

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { FaJs, FaReact } from 'react-icons/fa';
+import { FaHtml5, FaCss3, FaJs, FaDatabase, FaNode, FaNpm, FaBootstrap, FaReact, FaAws, FaSlack } from 'react-icons/fa';
 import "./skills.css" 
 
 function Skills() {
   return (
       <div>
-      <h1>Current languages</h1>
+      <h1>Skills and Expertises</h1>
       <ul>
-    <li> <FaReact /> </li>
+    <li> <FaHtml5 /> </li>
+    <li> <FaCss3 /> </li>
     <li> <FaJs /> </li>
+    <li> <FaDatabase /> </li>
+    <li> <FaNpm /> </li>
+    <li> <FaNode /> </li>
+    <li> <FaBootstrap /> </li>
+    <li> <FaReact /> </li>
+    <li> <FaAws /> </li>
+    <li> <FaSlack /> </li>
+
 </ul>
 </div>
   )
