729740d4ccb8718223b19b38da932c9d0cd8a25a
Martin Renteria <renteriamp96@gmail.com>
2021-07-21 07:23:03 -0700

    Added about section and added additional styling to section tags

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Home from "./components/Home/home";
 import NavBar from "./components/NavBar/navbar";
 import Skills from './components/Skills/skills';
 import Projects from './components/Projects/project';
+import About from './components/About/about';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Home />
+      <About />
       <Projects />
       <Skills />
       <Contact />
---
+++ src/components/About/about.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./about.css"
+
+function About() {
+    return(
+        <section class="about">
+            <h2 class="about-title">About Me</h2>
+            <div>
+            <p>I love problem solving and meeting new challenges. The feeling of completing new projects from scartch brings opportunities for growth and adds to my accomplishments as software engineer.
+                When am not building websites and apps, you'll find me hiking, playing the piano and spending time with my family.
+            
+                Currently looking for my next opportunity. Feel free to contact me.
+                </p>
+        </div>
+        </section>
+        
+    )
+}
+
+export default About;
