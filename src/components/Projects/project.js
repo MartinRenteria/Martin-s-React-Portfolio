7c49b8db747bac496867198541456113a4ada344
Martin Renteria <renteriamp96@gmail.com>
2021-07-22 12:15:13 -0700

    Reorganized image files within components and added project image

--- src/components/Projects/project.js
+++ src/components/Projects/project.js
@@ -1,11 +1,15 @@
 import React from "react";
-import "./project.css"
+import "./project.css";
+import Google from "./images/GoogleReact.png"
 
 function Projects() {
     return (
+        <main>
         <div>
         <h1 className="Project-title">Projects</h1>
         </div>
+                    <img src={Google} class="project-images" alt="Screenshot of project"></img>
+                </main>
     )
 }
 
