575fe5a9668389b51a2b3852c93ab3f46a0fd7cb
Martin Renteria <renteriamp96@gmail.com>
2021-08-05 12:07:10 -0700

    Adding overlay effet to project images

--- src/components/Projects/project.js
+++ src/components/Projects/project.js
@@ -8,7 +8,15 @@ function Projects() {
         <div>
         <h1 id="project-title">Projects</h1>
         </div>
-                    <img src={Google} class="project-images" alt="Screenshot of project"></img>
+            <div className="container">
+                <div className="overlay">
+                    <img src={Google} class="project-images" alt="Screenshot of project - Google Books built with React framework"></img>
+                    </div>
+                    <div className="middle">
+                    <div className="project-links">GitHub Repo</div>
+                    <div className="project-links">Live Site</div>
+                    </div>
+            </div>
         </section>
     )
 }
