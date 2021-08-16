777b9a438e73495b52338aa08fdaa894fb33c561
Martin Renteria <renteriamp96@gmail.com>
2021-08-16 12:16:33 -0700

    Fixing about me typo and adding wildcard reset values

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -7,8 +7,8 @@ function About() {
 			<h2 className="about-title">About Me</h2>
 			<div>
 				<p className="aboutContent">
-					I love problem solving and meeting new challenges. The feeling of completing new projects from
-					scartch brings opportunities for growth and adds to my accomplishments as software engineer. When am
+					I love problem solving and taking new challenges. The feeling of completing new projects from
+					scartch brings opportunities for growth and adds to my accomplishments as a software engineer. When am
 					not building websites and apps, you'll find me hiking, playing the piano and spending time with my
 					family. Currently looking for my next opportunity. Feel free to contact me.
 				</p>
