19c743d025fa77526724343b4c3fb862160e8f8c
Martin Renteria <renteriamp96@gmail.com>
2021-08-17 08:35:09 -0700

    Adding images to folders and updating about me section

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -7,10 +7,13 @@ function About() {
 			<h2 className="about-title">About Me</h2>
 			<div>
 				<p className="aboutContent">
-					I love problem solving and taking new challenges. The feeling of completing new projects from
-					scartch brings opportunities for growth and adds to my accomplishments as a software engineer. When am
-					not building websites and apps, you'll find me hiking, playing the piano and spending time with my
-					family. Currently looking for my next opportunity. Feel free to contact me.
+					<br />
+					I love to understand how things work, solving problems and taking on new challenges. <br />
+					<br />The feeling of completing new projects from scartch brings opportunities for growth and adds
+					to my accomplishments as a software engineer.<br />
+					<br /> When am not building websites and apps, you'll find me hiking, playing the piano and spending
+					time with my family. <br />
+					<br />Currently looking for my next opportunity. Feel free to contact me.
 				</p>
 			</div>
 		</section>
