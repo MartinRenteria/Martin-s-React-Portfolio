acf3806a71a29b92a60b27d375be9d9648bfa463
Martin Renteria <renteriamp96@gmail.com>
2021-08-23 17:29:32 -0700

    Aligned project divs to desired locations on project section

--- src/components/Projects/project.js
+++ src/components/Projects/project.js
@@ -4,10 +4,14 @@ import Google from "./images/GoogleReact.png";
 
 function Projects() {
 	return (
-		<section className="projects">
+		<section>
+
+			{/*Project 1 */}
+
 			<div>
 				<h1 id="project-title">Projects</h1>
 			</div>
+		<div className="projects">
 			<div className="projectContainer">
 				<div className="containerBox">
 					<img
@@ -39,6 +43,182 @@ function Projects() {
 						</div>
 					</div>
 				</div>
+			</div>
+
+			{/*Project 2 */}
+
+			<div className="projectContainer">
+				<div className="containerBox">
+					<img
+						src={Google}
+						className="project-pictures"
+						alt="Screenshot of project - Google Books built with React framework"
+					/>
+					<div className="overlay">
+						<p className="proj-description">
+							A Full-Stack MERN project (MongoDB database, Express.JS, React. JS, and Node.JS) made with
+							the Google Books API that searches all books available from Google and then save them to a
+							list.
+						</p>
+						<div className="proj-button-container">
+						<button className="siteButtons">
+							<a href="https://google-books-react-api.herokuapp.com/" target="_blank" rel="noreferrer">
+								Link Site
+							</a>
+						</button>
+						<button className="siteButtons">
+							<a
+								href="https://github.com/MartinRenteria/Google-Book-Search-With-React"
+								target="_blank"
+								rel="noreferrer"
+							>
+								GitHub
+							</a>
+						</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{/*Project 3 */}
+
+			<div className="projectContainer">
+				<div className="containerBox">
+					<img
+						src={Google}
+						className="project-pictures"
+						alt="Screenshot of project - Google Books built with React framework"
+					/>
+					<div className="overlay">
+						<p className="proj-description">
+							A Full-Stack MERN project (MongoDB database, Express.JS, React. JS, and Node.JS) made with
+							the Google Books API that searches all books available from Google and then save them to a
+							list.
+						</p>
+						<div className="proj-button-container">
+						<button className="siteButtons">
+							<a href="https://google-books-react-api.herokuapp.com/" target="_blank" rel="noreferrer">
+								Link Site
+							</a>
+						</button>
+						<button className="siteButtons">
+							<a
+								href="https://github.com/MartinRenteria/Google-Book-Search-With-React"
+								target="_blank"
+								rel="noreferrer"
+							>
+								GitHub
+							</a>
+						</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{/*Project 4 */}
+
+			<div className="projectContainer">
+				<div className="containerBox">
+					<img
+						src={Google}
+						className="project-pictures"
+						alt="Screenshot of project - Google Books built with React framework"
+					/>
+					<div className="overlay">
+						<p className="proj-description">
+							A Full-Stack MERN project (MongoDB database, Express.JS, React. JS, and Node.JS) made with
+							the Google Books API that searches all books available from Google and then save them to a
+							list.
+						</p>
+						<div className="proj-button-container">
+						<button className="siteButtons">
+							<a href="https://google-books-react-api.herokuapp.com/" target="_blank" rel="noreferrer">
+								Link Site
+							</a>
+						</button>
+						<button className="siteButtons">
+							<a
+								href="https://github.com/MartinRenteria/Google-Book-Search-With-React"
+								target="_blank"
+								rel="noreferrer"
+							>
+								GitHub
+							</a>
+						</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{/*Project 5 */}
+
+			<div className="projectContainer">
+				<div className="containerBox">
+					<img
+						src={Google}
+						className="project-pictures"
+						alt="Screenshot of project - Google Books built with React framework"
+					/>
+					<div className="overlay">
+						<p className="proj-description">
+							A Full-Stack MERN project (MongoDB database, Express.JS, React. JS, and Node.JS) made with
+							the Google Books API that searches all books available from Google and then save them to a
+							list.
+						</p>
+						<div className="proj-button-container">
+						<button className="siteButtons">
+							<a href="https://google-books-react-api.herokuapp.com/" target="_blank" rel="noreferrer">
+								Link Site
+							</a>
+						</button>
+						<button className="siteButtons">
+							<a
+								href="https://github.com/MartinRenteria/Google-Book-Search-With-React"
+								target="_blank"
+								rel="noreferrer"
+							>
+								GitHub
+							</a>
+						</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{/*Project 6 */}
+
+			<div className="projectContainer">
+				<div className="containerBox">
+					<img
+						src={Google}
+						className="project-pictures"
+						alt="Screenshot of project - Google Books built with React framework"
+					/>
+					<div className="overlay">
+						<p className="proj-description">
+							A Full-Stack MERN project (MongoDB database, Express.JS, React. JS, and Node.JS) made with
+							the Google Books API that searches all books available from Google and then save them to a
+							list.
+						</p>
+						<div className="proj-button-container">
+						<button className="siteButtons">
+							<a href="https://google-books-react-api.herokuapp.com/" target="_blank" rel="noreferrer">
+								Link Site
+							</a>
+						</button>
+						<button className="siteButtons">
+							<a
+								href="https://github.com/MartinRenteria/Google-Book-Search-With-React"
+								target="_blank"
+								rel="noreferrer"
+							>
+								GitHub
+							</a>
+						</button>
+						</div>
+					</div>
+				</div>
+			</div>
 			</div>
 		</section>
 	);
